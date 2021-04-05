78ad0b40805fc356ffaea442919e863c81a26880
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-04-05 20:32:47 +0300

    Create Song.js

---
+++ src/components/Song.js
@@ -0,0 +1,29 @@
+import React from "react";
+import songs from "../data/songs.json";
+
+function Song(props) {
+ const id = props.match.params.id;
+ const song = songs.find((song) => song.id === id);
+ return (
+  <div>
+   <h1>{song.name}</h1>
+   <h2>{song.artist}</h2>
+   <h3>{song.album}</h3>
+   <h4>{song.length}</h4>
+
+   <iframe
+    width="560"
+    height="315"
+    src={song.youtube_link}
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+   ></iframe>
+   <h2>lyrics:</h2>
+   <div className="lyrics">{song.lyrics}</div>
+  </div>
+ );
+}
+
+export default Song;
