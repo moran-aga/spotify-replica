391bf97bee4b19bb359c13e2d215d8c5dd325639
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-04-05 20:31:56 +0300

    home page component

---
+++ src/components/Home.js
@@ -0,0 +1,76 @@
+import React from "react";
+import albums from "../data/albums.json";
+import artists from "../data/artists.json";
+import songs from "../data/songs.json";
+import playlists from "../data/playlists.json";
+import { Link } from "react-router-dom";
+
+function Home(props) {
+ return (
+  <div>
+   <h2 className="sliderHeader">TOP PLAYLISTS</h2>
+   <ul className="slider">
+    {playlists.map((playlist) => {
+     return (
+      <>
+       <div className="sliderItem">
+        <Link to={`/playlist/${playlist.id}`}>
+         {playlist.cover_img && (
+          <img className="img" src={`${playlist.cover_img}`} />
+         )}
+         <li>{playlist.name}</li>
+        </Link>
+       </div>
+      </>
+     );
+    })}
+   </ul>
+   <h2 className="sliderHeader">TOP SONGS</h2>
+   <ul className="slider">
+    {songs.slice(0, 5).map((song) => {
+     return (
+      <>
+       <div className="sliderItem">
+        <Link to={`/song/${song.id}`}>
+         {song.cover_img && <img className="img" src={`${song.cover_img}`} />}
+         <li>{song.name}</li>
+        </Link>
+       </div>
+      </>
+     );
+    })}
+   </ul>
+   <h2 className="sliderHeader">TOP ARTISTS</h2>
+   <ul className="slider">
+    {artists.map((artist) => {
+     return (
+      <div className="sliderItem">
+       <Link to={`/artist/${artist.id}`}>
+        {artist.cover_img && (
+         <img className="img" src={`${artist.cover_img}`} />
+        )}
+        <li>{artist.name}</li>
+       </Link>
+      </div>
+     );
+    })}
+   </ul>
+
+   <h2 className="sliderHeader">TOP ALBUMS</h2>
+   <ul className="slider">
+    {albums.map((album) => {
+     return (
+      <div className="sliderItem">
+       <Link to={`/album/${album.id}`}>
+        {album.cover_img && <img className="img" src={`${album.cover_img}`} />}
+        <li>{album.name}</li>
+       </Link>
+      </div>
+     );
+    })}
+   </ul>
+  </div>
+ );
+}
+
+export default Home;
